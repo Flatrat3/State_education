3258b40585790109385ba8330ea02b4ec27d22c3
Flatrat3 <rocco.borocco95@gmail.com>
2026-01-18 18:36:56 +0400

    scroll problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -16,6 +17,7 @@ import TeachersLegislation from './pages/TeachersLegislation';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Header />
         <main>
